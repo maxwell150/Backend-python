b59c3a39e3db833a00efb404465a55c56b0ce421
max <maxwelgithumbi@gmail.com>
2022-04-14 20:41:00 +0300

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types-lis of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Return sum float'''
+    return sum(input_list)
